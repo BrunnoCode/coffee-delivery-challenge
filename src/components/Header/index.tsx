26773fa51a5a6290562c68dbbfae561260c24891
unknown <brunnowr.bw@gmail.com>
2023-04-25 18:50:57 +0200

    header component creation and defaultlayout for the pages using styled-components, phosphor-react and routes creation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
-import { HeaderContainer } from "./styled";
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from "./styled";
 import  logoCoffee  from "../../assets/logo.svg";
+import { MapPin, ShoppingCart } from "phosphor-react";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <div>
+      <div className="container">
         <img src={logoCoffee} />
+
+        <HeaderButtonsContainer>
+        <HeaderButton>
+        <MapPin size={20} weight="fill" />
+        Belo Horizonte, MG
+        </HeaderButton>
+        </HeaderButtonsContainer>
+
       </div>
     </HeaderContainer>
   );
