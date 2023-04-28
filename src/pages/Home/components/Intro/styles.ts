ef81197f7e30c40babdbb8120d75034d463a3459
unknown <brunnowr.bw@gmail.com>
2023-04-28 18:34:13 +0200

    update with icons section components and a specific typography styles

--- src/pages/Home/components/Intro/styles.ts
+++ src/pages/Home/components/Intro/styles.ts
@@ -1,15 +1,31 @@
 import styled from "styled-components";
 import BackgroundIntro from '../../../../assets/Background.png'
+import { TitleText } from "../../../../components/Typography";
 
 export const IntroContainer = styled.section`
   width: 100%;
   height: 34rem;
   background: ${() => `url(${BackgroundIntro})no-repeat center`};
   background-size: cover;
+
+  display: flex;
+  place-content: center;
 `
 export const IntroContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   gap: 3.5rem;
+`
+
+export const IntroTitle = styled(TitleText)`
+ margin-bottom: 1rem;
+`
+
+export const BenefitsContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  row-gap: 1.25rem;
+  margin-top: 4rem;
 `
